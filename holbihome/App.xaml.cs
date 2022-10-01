b2a6b9f90e2a8b39edf98f16cacbde3bdc31bae7
Peyton Smith <peytonbrsmith@gmail.com>
2022-10-01 00:03:30 -0500

    added project

---
+++ holbihome/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace holbihome;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ holbihome/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace holbihome;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
